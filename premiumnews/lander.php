30e70a3f8009f2b68a6d270a90ecaf558a95a379
Bryan Duckworth <bryanduckworth@gmail.com>
2013-07-16 23:48:23 -0700

    create new temporary landing page

---
+++ premiumnews/lander.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Template Name: Lander
+*/
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>WAPA - We Are Party Animals - UNDER CONSTRUCTION!</title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/styles/style.css">
+    <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body id="home">
+
+	<div class="WAPAlogo">
+		<img src="<?php echo get_template_directory_uri(); ?>/images/lander/WAPA-650px.png" />
+	</div>
+   	<div class="bottom"> 
+	    <h1>UNDER CONSTRUCTION</h1>
+	    <h2><a href="/stuff">Continue to the Old Site</a>
+    </div>
+
+</body>
+</html>
